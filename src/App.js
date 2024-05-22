e44c87509a5baa1867aed544adba5855406d33ac
Checo894 <A01068505@tec.mx>
2024-05-22 10:27:38 -0600

    carpeta backend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchBar from './Pages/SearchBar';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<SearchBar />} />
