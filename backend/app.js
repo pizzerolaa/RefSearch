426e2d70e9cb0d99ec1552a489f55cafb2024ea9
Fher García <fernanAnt.0711@hotmail.com>
2024-05-23 12:13:38 -0600

    backend3

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
 
-app.listen(8800, ()=> {
-    console.log('Backend server is running!');
-})
+const db = mysql.createConnection({
+    host: 'local',
+    user: 'root',
+    password: '',
+    database: 'db',
+});
+
+const PORT = 8800;
+
+app.listen(PORT, ()=> {
+    console.log('Server listening on port 8800');
+});
+//Hey se vale programas bien e
