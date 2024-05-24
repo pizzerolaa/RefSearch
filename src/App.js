22e3ac0af6c4f876b46324f3075afcb7ef4d539f
Checo894 <A01068505@tec.mx>
2024-05-23 21:41:26 -0600

    propuesta navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchBar from './Pages/SearchBar';
 import Login from './Pages/Login';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar /> */}
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<SearchBar />} />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,21 +4,24 @@ import user from "../Assets/user.svg";
 import idioma from "../Assets/globe.svg";
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <div class="navbar-login">
-            <button className='login-button'>
-                <img src={user} alt="login" />
-            </button>
+    return (
+        <div className="navbar">
+            <div className="navbar-functions">
+                <div class="navbar-login">
+                    <img src={user} alt="login" />   
+                </div>
+                <div className="navbar-idioma">
+                    <button>
+                        <img src={idioma} alt="" />
+                        <p>Idioma</p>
+                    </button>
+                </div>
+            </div>
+            <div className="navbar-logo">
+                <p>RefSearch</p>
+            </div>
         </div>
-        <div className="navbar-idioma">
-            <button>
-                <img src={idioma} alt="" />
-                <p>Idioma</p>
-            </button>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
