8385b8616380420bc7bfa7a5c66244a8754c0fd5
Checo894 <A01068505@tec.mx>
2024-05-23 21:50:18 -0600

    layout prompts (starting)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchBar from './Pages/SearchBar';
-import Login from './Pages/Login';
 import Navbar from './Components/Navbar/Navbar';
+import Login from './Pages/Login';
+import Prompts from './Pages/Prompts';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<SearchBar />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/prompts" element={<Prompts />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/Prompts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Prompts.css';
+
+const Prompts = () => {
+    return (
+      <div className='prompts'>
+        {/* Your code here */}
+      </div>
+    );
+ }
+
+
+export default Prompts;
