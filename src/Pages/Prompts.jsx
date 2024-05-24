3e55cff019dc30538c26f196dc0d640891201f32
Checo894 <A01068505@tec.mx>
2024-05-23 23:39:30 -0600

    propuesta prompts

--- src/Pages/Prompts.jsx
+++ src/Pages/Prompts.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import './styles/Prompts.css';
+import Redo from "../Components/Assets/redo.svg";
 
 const Prompts = () => {
     return (
-      <div className='prompts'>
-        {/* Your code here */}
-      </div>
+        <div className='prompts'>
+            <h1>
+                ¿Qué idea te agrada más?
+            </h1>
+            <div className="prompts-row1">
+                <button>
+                    Historia del arte digital
+                </button>
+                <button>
+                    Realidad aumentada en el arte
+                </button>
+            </div>
+            <div className="prompts-row2">
+                <button>
+                    Inteligencia artificial y arte
+                </button>
+                <button>
+                    Arte generativo
+                </button>
+            </div>
+            <button>
+                Arte y realidad virtual
+            </button>
+            <div className="prompts-redo">
+                <button>
+                    <img src={Redo} alt="" />
+                </button>
+            </div>
+        </div>
     );
- }
+}
 
 
 export default Prompts;
