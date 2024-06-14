bbbf7c407decf46d0ad40361782581f61a023609
Fher García <fernanAnt.0711@hotmail.com>
2024-06-13 23:42:00 -0600

    finalCommitFher

--- src/Pages/Results.jsx
+++ src/Pages/Results.jsx
@@ -1,5 +1,3 @@
-// Results.jsx
-
 import React, { useState, useEffect } from 'react';
 import './styles/Results.css';
 import { Link } from 'react-router-dom';
@@ -18,31 +16,31 @@ const Results = () => {
         username: '',
         reference: ''
     });
-  
-    const textToTranslate = {
-    references: "Lista de referencias"
-  };
 
-  const translateText = async (textsToTranslate = textToTranslate) => {
-    try {
-      const response = await axios.post('http://localhost:8800/translate', {
-        text: Object.values(textsToTranslate).join('\n'),
-        targetLang: language,
-      });
-      const translations = response.data.translations[0].text.split('\n');
-      const translatedObject = Object.keys(textsToTranslate).reduce((acc, key, index) => {
-        acc[key] = translations[index];
-        return acc;
-      }, {});
-      setTranslatedText(translatedObject);
-    } catch (error) {
-      console.error('Error al traducir:', error);
-    }
-  };
-  
-  useEffect(() => {
-    translateText();
-  }, [language]);
+    const textToTranslate = {
+        references: "Lista de referencias"
+    };
+
+    const translateText = async (textsToTranslate = textToTranslate) => {
+        try {
+            const response = await axios.post('http://localhost:8800/translate', {
+                text: Object.values(textsToTranslate).join('\n'),
+                targetLang: language,
+            });
+            const translations = response.data.translations[0].text.split('\n');
+            const translatedObject = Object.keys(textsToTranslate).reduce((acc, key, index) => {
+                acc[key] = translations[index];
+                return acc;
+            }, {});
+            setTranslatedText(translatedObject);
+        } catch (error) {
+            console.error('Error al traducir:', error);
+        }
+    };
+
+    useEffect(() => {
+        translateText();
+    }, [language]);
 
     useEffect(() => {
         // Recuperar el prompt del local storage y eliminar las comillas
@@ -55,16 +53,15 @@ const Results = () => {
         const fetchData = async () => {
             const results = localStorage.getItem('scholarlyResults');
             if (results) {
-              const parsedResults = JSON.parse(results);
-              const convertedResults = parsedResults.map(article => ({
-                  ...article,
-                  title: convertSpecialCharacters(article.title),
-                  summary: convertSpecialCharacters(article.summary),
-                  authors: article.authors.map(author => convertSpecialCharacters(author))
-              }));
-              setArticles(convertedResults);
+                const parsedResults = JSON.parse(results);
+                const convertedResults = parsedResults.map(article => ({
+                    ...article,
+                    title: convertSpecialCharacters(article.title),
+                    summary: convertSpecialCharacters(article.summary),
+                    authors: article.authors.map(author => convertSpecialCharacters(author))
+                }));
+                setArticles(convertedResults);
             }
-            // setLoading(false);
         };
         fetchData();
     }, []);
@@ -72,65 +69,64 @@ const Results = () => {
     const convertSpecialCharacters = (text) => {
         if (!text) return '';
         return text.replace(/\\u00e1/g, 'á')
-                   .replace(/\\u00e9/g, 'é')
-                   .replace(/\\u00ed/g, 'í')
-                   .replace(/\\u00f3/g, 'ó')
-                   .replace(/\\u00fa/g, 'ú')
-                   .replace(/\\u00f1/g, 'ñ')
-                   .replace(/\\u00c1/g, 'Á')
-                   .replace(/\\u00c9/g, 'É')
-                   .replace(/\\u00cd/g, 'Í')
-                   .replace(/\\u00d3/g, 'Ó')
-                   .replace(/\\u00da/g, 'Ú')
-                   .replace(/\\u00d1/g, 'Ñ')
-                   .replace(/\\u00fc/g, 'ü')
-                   .replace(/\\u00dc/g, 'Ü')
-                   .replace(/{\\'\\i}/g, 'í')
-                   .replace(/{\\'a}/g, 'á')
-                   .replace(/{\\'e}/g, 'é')
-                   .replace(/{\\'o}/g, 'ó')
-                   .replace(/{\\'u}/g, 'ú')
-                   .replace(/{\\~n}/g, 'ñ')
-                   .replace(/{\\'A}/g, 'Á')
-                   .replace(/{\\'E}/g, 'É')
-                   .replace(/{\\'I}/g, 'Í')
-                   .replace(/{\\'O}/g, 'Ó')
-                   .replace(/{\\'U}/g, 'Ú')
-                   .replace(/{\\~N}/g, 'Ñ');
+            .replace(/\\u00e9/g, 'é')
+            .replace(/\\u00ed/g, 'í')
+            .replace(/\\u00f3/g, 'ó')
+            .replace(/\\u00fa/g, 'ú')
+            .replace(/\\u00f1/g, 'ñ')
+            .replace(/\\u00c1/g, 'Á')
+            .replace(/\\u00c9/g, 'É')
+            .replace(/\\u00cd/g, 'Í')
+            .replace(/\\u00d3/g, 'Ó')
+            .replace(/\\u00da/g, 'Ú')
+            .replace(/\\u00d1/g, 'Ñ')
+            .replace(/\\u00fc/g, 'ü')
+            .replace(/\\u00dc/g, 'Ü')
+            .replace(/{\\'\\i}/g, 'í')
+            .replace(/{\\'a}/g, 'á')
+            .replace(/{\\'e}/g, 'é')
+            .replace(/{\\'o}/g, 'ó')
+            .replace(/{\\'u}/g, 'ú')
+            .replace(/{\\~n}/g, 'ñ')
+            .replace(/{\\'A}/g, 'Á')
+            .replace(/{\\'E}/g, 'É')
+            .replace(/{\\'I}/g, 'Í')
+            .replace(/{\\'O}/g, 'Ó')
+            .replace(/{\\'U}/g, 'Ú')
+            .replace(/{\\~N}/g, 'Ñ');
     };
 
-    const handleArticleClick =  (article) => {
+    const handleArticleClick = (article) => {
         localStorage.setItem('selectedArticle', JSON.stringify(article));
-    }
+    };
 
     const handleArticleSave = async (article) => {
         localStorage.setItem('savedArticle', JSON.stringify(article));
-        
+
         const username = localStorage.getItem('username');
         const savedArticle = localStorage.getItem('savedArticle');
-    
+
         const data = {
             username: username || '',
             reference: savedArticle || ''
         };
-    
+
         setFormData({
             username: username || '',
             reference: savedArticle || ''
         });
-    
+
         try {
             const response = await axios.post('http://localhost:8800/add-reference', data);
             console.log('Response:', response.data);
             alert('Reference added successfully!');
-            localStorage.removeItem('savedArticle');  // Optionally clear the saved article
+            localStorage.removeItem('savedArticle');  // Clear the saved article
         } catch (error) {
             console.error('Error adding reference:', error.response ? error.response.data : error.message);
             alert('Failed to add reference');
         }
-    }
-    
-    
+    };
+
     return (
         <div className='results'>
             <div className="results-results">
@@ -140,16 +136,16 @@ const Results = () => {
                         <div key={index} className="results-card">
                             <Link 
                                 to='/source'
-                                style={{textDecoration:'none'}}
+                                style={{ textDecoration: 'none' }}
                                 onClick={() => handleArticleClick(article)}
                             >
                                 <h2>{article.title}</h2>
                             </Link>
                             <div className="results-content">
-                                <div className="results-sumary">
-                                    <p>{article.summary || 'Not summary available. This might be a recent paper or preprint.'}</p>
+                                <div className="results-summary">
+                                    <p>{article.summary || 'No summary available. This might be a recent paper or preprint.'}</p>
                                 </div>
-                                <button onClick={()=>handleArticleSave(article)}>
+                                <button onClick={() => handleArticleSave(article)}>
                                     <img src={Ref} alt="" />
                                 </button>
                             </div>
@@ -158,7 +154,7 @@ const Results = () => {
                 </div>
             </div>
             <div className="results-reflist">
-                <Link style={{textDecoration:'none'}} to='/references'>
+                <Link style={{ textDecoration: 'none' }} to='/references'>
                     <button id='results-reflist'>
                         <span>{translatedText.references}</span>
                         <img src={Next} alt="" />
@@ -170,122 +166,3 @@ const Results = () => {
 }
 
 export default Results;
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import './styles/Results.css';
-// import { Link } from 'react-router-dom';
-// import Globe from '../Components/Assets/globe-beish.svg';
-// import Ref from '../Components/Assets/bookmark.svg';
-// import Next from '../Components/Assets/arrowNext.svg';
-
-// const Results = () => {
-
-//     const [selectedPrompt, setSelectedPrompt] = useState('');
-//     const [articles, setArticles] = useState([]);
-
-//     useEffect(() => {
-//         // Recuperar el prompt del local storage y eliminar las comillas
-//         const prompt = localStorage.getItem('selectedPrompt');
-//         if (prompt) {
-//             const cleanedPrompt = prompt.replace(/['"]+/g, '');
-//             setSelectedPrompt(cleanedPrompt);
-//         }
-
-//         const fetchData = async () => {
-//             const results = localStorage.getItem('scholarlyResults');
-//             if (results) {
-//               const parsedResults = JSON.parse(results);
-//               setArticles(parsedResults);
-//             }
-//             // setLoading(false);
-//         };
-//         fetchData();
-//     }, []);
-
-//     const handleArticleClick = (article) => {
-//         localStorage.setItem('selectedArticle', JSON.stringify(article));
-//     }
-    
-//     return (
-//         <div className='results'>
-//             <div className="results-idioma">
-//                 <button id='results-idioma'>
-//                     <img src={Globe} alt="" />
-//                     <span>Idioma</span>
-//                 </button>
-//             </div>
-//             <div className="results-results">
-//                 <h1>{selectedPrompt}</h1>
-//                 <div className="results-container">
-//                     {articles.map((article, index) => (
-//                         <div key={index} className="results-card">
-//                             <Link 
-//                                 to='/source'
-//                                 style={{textDecoration:'none'}}
-//                                 onClick={() => handleArticleClick(article)}
-//                             >
-//                                 <h2>{article.title}</h2>
-//                             </Link>
-//                             <div className="results-content">
-//                                 <div className="results-sumary">
-//                                     <p>{article.summary || 'Not summary available. This might be a recent paper or preprint.'}</p>
-//                                 </div>
-//                                 <button>
-//                                     <img src={Ref} alt="" />
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     ))}
-//                 </div>
-//             </div>
-//             <div className="results-reflist">
-//                 <Link style={{textDecoration:'none'}} to='/references'>
-//                     <button id='results-reflist'>
-//                         <span>Lista de Referencias</span>
-//                         <img src={Next} alt="" />
-//                     </button>
-//                 </Link>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Results;
-
-// // {/* 
-// //     <div className="results-card">
-// //         <Link style={{textDecoration:'none'}} to='/source'>
-// //             <h2>Titulo del articulo</h2>
-// //         </Link>
-// //         <div className="results-content">
-// //             <div className="results-sumary">
-// //                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque mollitia adipisci quis vel autem architecto! Odit, pariatur enim quia aspernatur nam nobis! Ab reiciendis dignissimos quas recusandae impedit aliquam quia.</p>
-// //                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque mollitia adipisci quis vel autem architecto! Odit, pariatur enim quia aspernatur nam nobis! Ab reiciendis dignissimos quas recusandae impedit aliquam quia.</p>
-// //                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque mollitia adipisci quis vel autem architecto! Odit, pariatur enim quia aspernatur nam nobis! Ab reiciendis dignissimos quas recusandae impedit aliquam quia.</p>
-// //                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque mollitia adipisci quis vel autem architecto! Odit, pariatur enim quia aspernatur nam nobis! Ab reiciendis dignissimos quas recusandae impedit aliquam quia.</p>
-// //                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque mollitia adipisci quis vel autem architecto! Odit, pariatur enim quia aspernatur nam nobis! Ab reiciendis dignissimos quas recusandae impedit aliquam quia.</p>
-// //             </div>
-// //             <button>
-// //                 <img src={Ref} alt="" />
-// //             </button>
-// //         </div>
-// //     </div>
-// // */}
-
-// // {articles.map((article, index) => (
-// //     <div key={index} className="article-card">
-// //         <h3>{article.title}</h3>
-// //         <p><strong>Authors:</strong> {article.authors.join(', ')}</p>
-// //         <p><strong>Year:</strong> {article.year || 'Not available'}</p>
-// //         <p><strong>Summary:</strong> {article.summary || 'Not summary available. This might be a recent paper or preprint.'}</p>
-// //         <h3>
-// //           {article.link ? (
-// //             <a href={article.link} target="_blank" rel="noopener noreferrer">{article.title}</a>
-// //           ) : (
-// //             article.title
-// //           )}
-// //         </h3>
-// //     </div>
-// // ))}
--- src/Pages/SearchBar.jsx
+++ src/Pages/SearchBar.jsx
@@ -5,7 +5,7 @@ import './styles/SearchBar.css';
 import Plus from "../Components/Assets/plus.svg";
 import Lupa from "../Components/Assets/lupa.svg";
 
-const SearchBar = ({sharedVariable}) => {
+const SearchBar = ({ sharedVariable }) => {
 
   const [inputFields, setInputFields] = useState([""]);
   const navigate = useNavigate();
@@ -14,24 +14,20 @@ const SearchBar = ({sharedVariable}) => {
   const [randomPrompts, setRandomPrompts] = useState([]);
 
   useEffect(() => {
-    // Function to create the References table
     const createReferencesTable = async () => {
-        try {
-            const response = await axios.post('http://localhost:8800/create-references-table');
-            console.log('Response:', response.data);
-            if (response.data.message) {
-                console.log('Table creation message:', response.data.message);
-            }
-        } catch (error) {
-            console.error('Error creating References table:', error);
+      try {
+        const response = await axios.post('http://localhost:8800/create-references-table');
+        console.log('Response:', response.data);
+        if (response.data.message) {
+          console.log('Table creation message:', response.data.message);
         }
+      } catch (error) {
+        console.error('Error creating References table:', error);
+      }
     };
-
-    // Call the function to create the table
     createReferencesTable();
-}, []);
-  
-  
+  }, []);
+
   const textToTranslate = {
     title: '¿Sobre qué quieres investigar?',
     placeholder: 'Tema en mente',
@@ -59,11 +55,10 @@ const SearchBar = ({sharedVariable}) => {
       console.error('Error al traducir:', error);
     }
   };
-  
+
   useEffect(() => {
     translateText();
   }, [language]);
-
 
   const handleAddField = () => {
     if (inputFields.length < 5) {
@@ -84,9 +79,25 @@ const SearchBar = ({sharedVariable}) => {
       const response = await axios.post('http://localhost:8800/chat/prompts', { keywords: concatenatedString });
       console.log(response);
       navigate('/prompts');
-    }
-    catch (error) {
+    } catch (error) {
       console.error('Error fetching prompts: ', error);
+    }
+  };
+
+  const handlePromptClick = async (prompt) => {
+    try {
+      console.log('Selected prompt:', prompt);
+      localStorage.setItem('selectedPrompt', prompt);
+      window.alert('Buscando artículos académicos...');
+      const response = await axios.get('http://localhost:8800/scholarlyy', {
+        params: { q: prompt }
+      });
+      console.log('Response received:', response.data);
+      localStorage.setItem('scholarlyResults', JSON.stringify(response.data));
+      console.log('Datos guardados en localStorage');
+      navigate('/results');
+    } catch (error) {
+      console.error('Error fetching scholarly articles:', error);
     }
   };
 
@@ -95,23 +106,19 @@ const SearchBar = ({sharedVariable}) => {
       const response = await axios.get('http://localhost:8800/random-prompts');
       setRandomPrompts(response.data.prompts);
     } catch (error) {
-      console.error('Error fetching random prompts: ', error);
+      console.error('Error fetching random prompts:', error);
     }
   };
-  
+
   useEffect(() => {
     fetchRandomPrompts();
   }, []);
 
   return (
     <div className="searchBar">
-
       <div className='RefSearch-main'>
-
         <div className='rf-first'>
-
           <h2>{translatedText.title}</h2>
-          
           {inputFields.map((field, index) => (
             <div key={index} className='rf-first-search'>
               <input
@@ -125,32 +132,31 @@ const SearchBar = ({sharedVariable}) => {
               )}
             </div>
           ))}
-
           <h4>{translatedText.addKeyIdea}</h4>
           <div className="rf-first-enter">
-              <button onClick={handleButtonClick}>
-                <img src={Lupa} alt="" />
-              </button>
+            <button onClick={handleButtonClick}>
+              <img src={Lupa} alt="" />
+            </button>
           </div>
-          
         </div>
 
         <div className='rf-second'>
-
           <button>{translatedText.disc}</button>
-
           <div className='rf-second-buttons'>
-            {randomPrompts.map((prompt, index) => (
-              <Link key={index} style={{ textDecoration: 'none' }} to='/prompts'>
-                <button>{prompt}</button>
-              </Link>
-            ))}
+            <button onClick={() => handlePromptClick('Principios fundamentales de la física cuántica')}>
+              Principios fundamentales de la física cuántica
+            </button>
+            <button onClick={() => handlePromptClick('Metodologías modernas de enseñanza en educación primaria')}>
+              Metodologías modernas de enseñanza en educación primaria
+            </button>
+            <button onClick={() => handlePromptClick('Impacto del cambio climático en la biodiversidad global')}>
+              Impacto del cambio climático en la biodiversidad global
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
-  
 
 export default SearchBar;
